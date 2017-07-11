b9e817332b9bcd0c37f8e76629170a340f283142
Paul Hammant <paul@hammant.org>
2017-07-11 16:27:45 -0400

    embedded war file version of solution - works on Mac

--- src/test/mypackage/HWIntegrationTest.java
+++ src/test/mypackage/HWIntegrationTest.java
@@ -1,83 +1,44 @@
 package mypackage;
 
-import com.google.common.collect.ObjectArrays;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.Filter;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebListener;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.EventListener;
+import java.io.File;
 
 import static io.restassured.RestAssured.when;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.containsString;
 
 public class HWIntegrationTest {
 
-    private static void addListener(WebAppContext context, Class<?> type) throws InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException {
-        WebListener listenAnn = type.getAnnotation(WebListener.class);
-        if (listenAnn != null) {
-            context.addEventListener((EventListener) type
-                    .getConstructor().newInstance());
-        }
-    }
-
-    private static void addFilter(WebAppContext context, Class<?> type) throws InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException {
-        WebFilter filterAnn = type.getAnnotation(WebFilter.class);
-        if (filterAnn != null) {
-            for (String pattern : ObjectArrays.concat(
-                    filterAnn.value(), filterAnn.urlPatterns(),
-                    String.class)) {
-                DispatcherType[] disTypes = filterAnn.dispatcherTypes();
-                EnumSet<DispatcherType> dispatches = EnumSet
-                        .copyOf(Arrays.asList(disTypes));
-                context.addFilter(new FilterHolder((Filter) type
-                                .getConstructor().newInstance()), pattern,
-                        dispatches);
-            }
-        }
-    }
-
-    private static void addServlet(WebAppContext context, Class<?> type) throws InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException {
-        WebServlet servletAnn = type.getAnnotation(WebServlet.class);
-        if (servletAnn != null) {
-            for (String pattern : ObjectArrays.concat(
-                    servletAnn.value(), servletAnn.urlPatterns(),
-                    String.class)) {
-                context.addServlet(new ServletHolder((HttpServlet) type
-                        .getConstructor().newInstance()), pattern);
-            }
-        }
-    }
-
-
     @Before
     public void beforeAll() throws Exception {
+
+        System.out.println("cd: " + new File(".").getAbsolutePath());
+
         Server server = new Server(8080);
-        WebAppContext context = new WebAppContext();
-        context.setResourceBase("./web");
-        addServlet(context, HelloworldServlet.class);
-        server.setHandler(context);
+        // Configure webapp provided as external WAR
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        webapp.setWar("../sample.war");
+        server.setHandler(webapp);
+
+        // Start the server
         server.start();
+
     }
 
     @Test
-    public void testSomething() {
+    public void testSomething() throws InterruptedException {
+
+//        Thread.sleep(10000000);
 
         when()
-            .get("/helloworld")
+            .get("/hello")
         .then()
             .statusCode(200)
-            .body(equalTo("Hello, world\n"));
+            .body(containsString("Hello, World"));
     }
 
 }
